98f69e6d4feba3cc9775a6abb3c3e8d688d68734
Sean Golden <sean.golden.one@gmail.com>
2026-01-21 13:22:23 -0800

    update image

--- script.js
+++ script.js
@@ -24,16 +24,6 @@ function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
-// function showSlides(n, no) {
-//   let i;
-//   let x = document.getElementsByClassName(slideId[no]);
-//   if (n > x.length) {slideIndex[no] = 1}
-//   if (n < 1) {slideIndex[no] = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex[no]-1].style.display = "block";
-// }
 function showSlides(n, no) {
   let i;
   let slides = document.getElementsByClassName(slideId[no]);
